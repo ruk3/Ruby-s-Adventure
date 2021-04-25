51f040717a653646afa4a0668d0835f5e1d397c7
ruk3 <61035764+ruk3@users.noreply.github.com>
2021-04-25 10:01:50 -0400

    initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody2d;
+    float timer;
+    int direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed * Time.deltaTime * direction; ;
+        }
+        else
+        {
+            position.x = position.x + speed * Time.deltaTime * direction; ;
+        }
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+}
